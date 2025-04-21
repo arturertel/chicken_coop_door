3a703d8968ed115590c0c5490d71b8dd09e5ef92
arturertel <arturertel@gmail.com>
2025-04-21 19:15:27 +0300

    remove last_checked_day

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ![Image of the user interface](https://github.com/arturertel/chicken_coop_door/blob/main/img/userinterface.jpg?raw=true)
-![Image of the electronics on the back](https://github.com/arturertel/chicken_coop_door/blob/main/img/electronics.jpg?raw=true)
 ![Image of the motor and the limit switches](https://github.com/arturertel/chicken_coop_door/blob/main/img/motor.jpg?raw=true)
 
 Parts
--- automatic_loop.py
+++ automatic_loop.py
@@ -4,15 +4,16 @@ from new_motor import MotorDriver
 from settings.save_settings import Settings
 import global_state  # enthält mode_switch
 from machine import Pin
+
 ds = DS1302(Pin(18), Pin(17), Pin(16))
 motor = MotorDriver(pwm_pin=19, dir_pin=28, en_pin=20)
 
 already_opened = False
 already_closed = False
-last_checked_day = -1  # zum Tageswechsel erkennen
+
 
 async def automatic_loop():
-    global already_opened, already_closed, last_checked_day
+    global already_opened, already_closed
 
     while True:
         if global_state.mode_switch == 4:  # Automatikmodus aktiviert
@@ -20,37 +21,33 @@ async def automatic_loop():
 
             hour = ds.hour()  # Die aktuelle Stunde
             minute = ds.minute()  # Die aktuelle Minute
-            day = ds.day()  # Der aktuelle Tag des Monats
-            
-            print("AUTOMATIC LOOP läuft, mode =", global_state.mode_switch)
 
-            print("Uhrzeit:", ds.hour(), ":", ds.minute())
+            print("AUTOMATIC LOOP läuft, mode =", global_state.mode_switch)
+            print("Uhrzeit:", hour, ":", minute)
             print("Open-Zeit:", settings.open)
             print("Close-Zeit:", settings.close)
-            
-            # Prüfen, ob ein neuer Tag ist
-            if day != last_checked_day:
-                already_opened = False
-                already_closed = False
-                last_checked_day = day
-                print("Neuer Tag – Status zurückgesetzt")
 
             # Wenn es die Öffnungszeit ist und noch nicht geöffnet wurde
             if [hour, minute] == settings.open and not already_opened:
                 print("Automatik: Öffnen der Tür")
-                uasyncio.create_task(run_motor(1))  # Motor starten, Richtung 0
+                # Motor starten, Richtung 1 für Öffnen
+                uasyncio.create_task(run_motor(1))
                 already_opened = True
+                already_closed = False  # Status für Schließen zurücksetzen
 
             # Wenn es die Schließzeit ist und noch nicht geschlossen wurde
             elif [hour, minute] == settings.close and not already_closed:
                 print("Automatik: Schließen der Tür")
-                uasyncio.create_task(run_motor(0))  # Motor starten, Richtung 1
+                # Motor starten, Richtung 0 für Schließen
+                uasyncio.create_task(run_motor(0))
                 already_closed = True
+                already_opened = False  # Status für Öffnen zurücksetzen
 
-        await uasyncio.sleep(1)  # 1 Sekunde warten, bevor die Schleife wiederholt wird
+        # 1 Sekunde warten, bevor die Schleife wiederholt wird
+        await uasyncio.sleep(1)
+
 
 async def run_motor(direction):
     motor.stop()
     await uasyncio.sleep(1)  # Kurze Pause, bevor der Motor startet
     motor.start(direction=direction, max_pwm=30000)  # Motor starten
-
